19b52844eecfc4616fbc32e0f7f4a81739d30d93
Mateusz Lach <235910@edu.p.lodz.pl>
2022-04-20 00:54:03 +0200

    2. Poprawa formatowania

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,17 @@
 package edu.kis.vh.nursery;
-
+//linijki ktore ulegly zmianie 5, 12, 14, 15
 public class HanoiRhymer extends defaultCountingOutRhymer {
+//alt + strzalka powoduje zmiane otwartego okna w IntelliJ
+    int totalRejected = 0;
 
-int totalRejected = 0;
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public int reportRejected() {
-		return totalRejected;
-	}
-
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }

05be6eca2da5819495ed14a5dcab3dd9728621a6
Piotr Sitarek <235969@edu.p.lodz.pl>
2022-04-20 13:23:51 +0200

    5. Zmiana literałów na stałe

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,19 +5,23 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
+    public static final int MAX_I = 15;
+    public static final int MAX_J = 3;
+    public static final int BOUND = 20;
+
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
         DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < MAX_I; i++)
+            for (int j = 0; j < MAX_J; j++)
                 rhymers[j].countIn(i);
 
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = 1; i < MAX_I; i++)
+            rhymers[MAX_J].countIn(rn.nextInt(BOUND));
 
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
@@ -26,7 +30,7 @@ class RhymersDemo {
         }
 
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[MAX_J]).reportRejected());
 
     }
 
